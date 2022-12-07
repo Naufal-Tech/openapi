b9fc4ecebfe411e41c3ae1587bb6077f450f28cf
Naufal-Tech <naufal.geo11@gmail.com>
2022-12-07 21:15:58 +0700

    first commit

---
+++ new.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const qrcode = require('qrcode-terminal');
+const fs = require("fs");
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+async function responsePrompt (prompt) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0.6,
+    max_tokens: 2048,
+  });
+  return response.data.choices[0].text;
+}
+const SESSION_PATH = './wh_Session.json';
+let sessionData;
+if (fs.existsSync(SESSION_PATH)) {
+    sessionData = require(SESSION_PATH);}
+const clientWA = new Client({ authStrategy: new LocalAuth(), session: sessionData} );
+clientWA.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+clientWA.on('ready', () => {
+    console.log('Client is ready!');
+});
+clientWA.on("message", async msg => {
+    const prefix = "!";
+    const args = msg.body.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (msg.body[0] === prefix) { 
+        if (command == "ask"){
+            const prompt = args.join(" ");
+            const response = await responsePrompt(prompt);
+            msg.reply(response);
+          }}})
+clientWA.initialize();
